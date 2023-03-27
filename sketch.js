d9ad91cbcfa5a473e6639fd41e4af7fbc15a9a22
sreeponnu <128480058+sreeponnu@users.noreply.github.com>
2023-03-27 13:18:29 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,4 +7,14 @@ function draw(){
     if(keyIsDown(RIGHT_ARROW)){
    background("red")
     }
-}
+    if(keyIsDown(LEFT_ARROW)){
+    background("green")
+    }
+    if(keyIsDown(UP_ARROW)){
+   background("blue")
+    }
+    if(keyIsDown(DOWN_ARROW)){
+   background("yellow")
+    }
+
+ }
